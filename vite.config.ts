6b9b1bc417ae8e70a4ede369c75debfa5dd5fe18
Shady Emad <shady.emad@email.com>
2026-02-10 06:40:51 +0200

    deploy second time

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/Cartify/",
+
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
