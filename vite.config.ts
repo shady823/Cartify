3b53591cd50378c0411af282b7cc642e53da60a2
shady823 <shadyemadkhairi2003@gmail.com>
2026-02-10 19:05:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/Cartify/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,3 +12,4 @@ export default defineConfig({
     },
   },
 });
+
